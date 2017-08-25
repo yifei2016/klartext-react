1642b6d82afc8714eff0df88575998a7939131b6
Yifei Wang <hello.yifei@icloud.com>
2017-08-25 20:38:24 +0200

    response from server did not play on page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import App from '../App';
-import {actionSelectedWord} from '../actions/index';
 import Select from 'react-select';
+
 import 'react-select/dist/react-select.css';
 class Search extends Component {
   constructor(props) {
@@ -10,32 +10,27 @@ class Search extends Component {
     this.state = {
       select: this.props.selectedWord
     }
-    this.handleChange = this.handleChange.bind(this);
-     this.logChange = this.logChange.bind(this);
+    this.logChange = this.logChange.bind(this);
   }
- handleChange(e){
-   debugger
-   let action = actionSelectedWord(e.target.value);
-   this.props.dispatch(action)
- }
-  logChange(e) {
-
-    axios({
+  logChange(input) {
+    return axios({
       method:'get',
-      url:`http://localhost:8080/api/words/search?query=${this.state.select}`,
+      url:`http://localhost:8080/api/words/search?query=${input}`,
       headers: {'Accept': 'application/json'}
     })
     .then(function(response) {
-
+      return response.data.map(x=>x.value)
     })
   }
   render() {
     return (
+      <div>
       <Select.Async
-        value={this.state.select}
-         onChange={this.handleChange}
+
         loadOptions={this.logChange}
       />
+
+      </div>
     )
   }
 }
