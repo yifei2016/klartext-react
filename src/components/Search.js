fd678d535b71e17093bd3cd88d8344b01592584d
Yifei Wang <hello.yifei@icloud.com>
2017-08-26 22:07:33 +0200

    conditional rendering select and div

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,9 +8,14 @@ class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      select: this.props.selectedWord
+      selected: '',
+      display: 'none',
+      lang: '',
+      flag: '',
+      klass: ''
     }
     this.logChange = this.logChange.bind(this);
+    this.selectedOption = this.selectedOption.bind(this);
   }
   logChange(input) {
     return axios({
@@ -27,26 +32,65 @@ class Search extends Component {
       }
     })
   }
+  selectedOption(selected){
+    if(selected.lang === 'sv'){
+    this.setState({
+      selected: selected,
+      display: 'block',
+      klass: selected.klass,
+      flag: 'flag-icon flag-icon-se'
+    })
+  }else {
+    this.setState({
+      selected: selected,
+      display: 'block',
+      klass: selected.klass,
+      flag: 'flag-icon flag-icon-en'
+    })
+  }
+    var sendValue = selected.inflection || []
+    if(sendValue.indexOf(selected.value === -1)){
+      sendValue.unshift(selected.value)
+    }
+
+    return axios({
+      method:'get',
+      url:`http://localhost:8080/api/posts/search?query=${sendValue}`,
+      headers: {'Accept': 'application/json'}
+    })
+    .then(function(response) {
+
+    }.bind(this))
+  }
   render() {
-    return (
-      <div>
-      <Select.Async
+    if(this.state.display === 'none'){
+      debugger
+      return (<Select.Async
         labelKey='value'
         loadOptions={this.logChange}
-        onNewOptionClick={this.selectedOption}
-      />
-      <div className="card" style={{width:'50%'}}>
-
-          <div className="card-block">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">Some quick exampleake up the bulk of the card's content.</p>
-
+        onChange={this.selectedOption}
+      />)
+    }else if(this.state.display === 'block'){
+      debugger
+      return (
+        <div>
+          <Select.Async
+            labelKey='value'
+            loadOptions={this.logChange}
+            onChange={this.selectedOption}
+          />
+          <div className="card">
+            <div className="card-block">
+              <h4 className="card-title">fdf{this.state.selected}
+                <span className={this.state.flag}></span>
+                <span>{this.state.klass}</span></h4>
+                <p className="card-text">Some quthe card's content.</p>
+              </div>
+            </div>
           </div>
-        </div>
-
-      </div>
-    )
-  }
+        )
+      }
+    }
 }
 
 export default Search;
